ca6b25ad4962376498ed631e2fba429dec0d6917
muhammetcetinalp <156845748+muhammetcetinalp@users.noreply.github.com>
2024-02-08 13:44:10 +0300

    app.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const difficulty = form[1];
 const type = form[2];
 const indiv = document.querySelector(".questions_div_class");
 
-let trueAnswers = 0;
-let falseAnswers = 0;
-
 let gameStarted = false;
 
 document.querySelector("#button").addEventListener("click", () => {
@@ -23,7 +20,6 @@ document.querySelector("#button").addEventListener("click", () => {
                 .then((resp) => {
                     writeQuestions(resp);
                 });
-
         }
         else {
             alert("Give a number Please!");
@@ -41,36 +37,43 @@ function writeQuestions(resp) {
     nextButton.id = "Next1";
     document.body.append(nextButton);
     nextButton.addEventListener("click", () => {
-
-        currentIndex++;
         if (currentIndex <= myArray.length) {
-
             let answers = document.querySelectorAll(".ans");
             const arr = Object.values(answers);
             arr.forEach((index) => {
                 if (index.checked) {
                     if (index.value === "correct") {
-                        trueAnswers++;
+                        currentIndex++;
+                        const del = document.querySelector(".card");
+                        del.remove();
+                        addQuestion(myArray[currentIndex]);
+                        if (currentIndex === myArray.length) {
+                            nextButton.remove();
+                            endOfGame();
+                        }
                     }
                     else if (index.value === "false") {
-                        falseAnswers++;
+                        const falseQuestion = document.querySelector(".card");
+                        
+                        const messageDiv = document.createElement("div");
+                        messageDiv.textContent = "Wrong Answer";
+                        messageDiv.className = "wrong-answer-message";
+                        falseQuestion.append(messageDiv)
+
+                        setTimeout(() => {
+                            messageDiv.remove();
+                        }, 2000);
+                        index.remove();
                     }
                 }
             });
-
-            const del = document.querySelector(".card");
-            del.remove();
-            addQuestion(myArray[currentIndex]);
-            if (currentIndex === myArray.length) {
-                nextButton.remove();
-                endOfGame();
-            }
 
         }
 
     });
 
 }
+
 
 function addQuestion(element) {
     if (element != null) {
@@ -79,35 +82,61 @@ function addQuestion(element) {
 
         const h2 = document.createElement("h2");
         h2.textContent = element.question;
-        indiv.append(div);
-        div.append(h2);
-        const myArray = [element.correct_answer, ...element.incorrect_answers]
+        div.appendChild(h2);
+
+        const optionsDiv = document.createElement("div");
+        optionsDiv.id = "options";
+
+        const myArray = [element.correct_answer, ...element.incorrect_answers];
         shuffle(myArray);
-        myArray.forEach((myarray, index) => {
-            const answer = document.createElement("input");
-            answer.className = "ans";
-            answer.type = "radio";
-            answer.name = "name";
-            if (myarray === element.correct_answer) {
-                answer.value = "correct"
-            }
-            else {
-                answer.value = "false";
-            }
-            const answer_text = document.createElement("a");
-            answer_text.textContent = myarray;
-            div.append(answer_text);
-            div.append(answer);
-        })
+
+        myArray.forEach(answer => {
+            const optionDiv = document.createElement("div");
+            optionDiv.className = "option";
+
+            const radioButton = document.createElement("input");
+            radioButton.type = "radio";
+            radioButton.className = "ans";
+            radioButton.name = "answer";
+            radioButton.value = answer === element.correct_answer ? "correct" : "false";
+
+            const label = document.createElement("label");
+            label.textContent = answer;
+
+            optionDiv.appendChild(radioButton);
+            optionDiv.appendChild(label);
+            optionsDiv.appendChild(optionDiv);
+        });
+
+        div.appendChild(optionsDiv);
+        indiv.appendChild(div);
     }
+
 
 }
 
 function endOfGame() {
-    alert("End of the Quiz");
-    alert("Number of True Answers is : " + trueAnswers)
-    form.style.display = "block";
-    document.querySelector("#button").style.display = "block";
+    const div = document.createElement("div");
+    div.className = "card";
+
+    const h1 = document.createElement("h1");
+    h1.textContent = "You've Reached End Of The Game";
+
+    div.append(h1)
+    indiv.append(div)
+
+    const NewGameButton = document.createElement("button");
+    NewGameButton.textContent = "NewGame    ";
+    NewGameButton.id = "NewGame";
+    document.body.append(NewGameButton);
+    NewGameButton.addEventListener("click", () => {
+        div.remove();
+        NewGameButton.remove();
+        form.style.display = "block";
+        document.querySelector("#button").style.display = "block";
+        gameStarted = false;
+    })
+
 
 }
 function shuffle(array) {
@@ -116,7 +145,3 @@ function shuffle(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
-
-
-
-
